27018319910c29a24ba405d3afbf644d9ae9bf96
Chris (Someguy123) <info@someguy123.com>
2020-06-22 02:18:10 +0100

    [WIP] Added support for hive-js, refactoring, plus WIP signing key support
    Work-in-progress commit. Currently does not work unless you change app.js:212 to `accountmgr.publish_feed(price);` for legacy feed publishing.
    
     - Refactored a lot of stuff into `lib/chain.js` from `app.js`
     - Unified `steem-js` and `hive-js` using `Blockchain` wrapper class in `lib/chain.js`
     - Support for multiple API nodes
     - Work-in-progress support for updating witness feeds using signing keys via witness_set_properties instead of feed_publish
     - New `lib/packer.js` module, related to low level packing and unpacking of Graphene byte structs - used for witness_set_properties
       until steem-js/hive-js have transparent automatic struct packing.
     - Added `@aksel/structjs` dependency, used for `lib/packer.js` for handling structs similar to the Python Beem library by @Holger80
     - Various other changes

--- app.js
+++ app.js
@@ -10,11 +10,18 @@
 
 var settings = require('./lib/settings');
 var exchange = require('./lib/exchange');
+var chain = require('./lib/chain');
 var request = require('request');
-var steem = require('steem');
+// var steem = require('steem');
 
 var config = settings.config,
     retry_conf = settings.retry_conf;
+
+const Account = chain.Account, 
+      Blockchain = chain.Blockchain, 
+      KeyPair = chain.KeyPair;
+
+// import {Account, Blockchain, KeyPair} from './lib/chain';
 
 console.log('-------------');
 log(`Loaded configuration:
@@ -30,146 +37,153 @@ for(var t_arg of process.argv) {
     }
 }
 
-steem.api.setOptions({ url: config.node });
+// steem.api.setOptions({ url: config.node });
 
-// used for re-trying failed promises
-function delay(t) {
-    return new Promise((r_resolve) => setTimeout(r_resolve, t) );
-}
+// // used for re-trying failed promises
+// function delay(t) {
+//     return new Promise((r_resolve) => setTimeout(r_resolve, t) );
+// }
 
-class SteemAcc {
-    constructor(username, active_wif) {
-        /**
-         * Initialises object with username and active private key
-         * @param  {string}  username    The username (without @)
-         * @param  {string}  active_wif  The active private key
-         * @throws {Error<string:msg>}   If private key is invalid
-         */
-        if(!steem.auth.isWif(config.wif)) {
-            throw new Error("The private key you specified is not valid. Be aware Steem private keys start with a 5.");
-        }
-        this.user_data = {username, active_wif};
-        this.wif_valid = null;
+// class SteemAcc {
+//     constructor(username, active_wif) {
+//         /**
+//          * Initialises object with username and active private key
+//          * @param  {string}  username    The username (without @)
+//          * @param  {string}  active_wif  The active private key
+//          * @throws {Error<string:msg>}   If private key is invalid
+//          */
+//         if(!steem.auth.isWif(config.wif)) {
+//             throw new Error("The private key you specified is not valid. Be aware Steem private keys start with a 5.");
+//         }
+//         this.user_data = {username, active_wif};
+//         this.wif_valid = null;
         
-    }
+//     }
 
-    loadAccount(reload=false, tries=0) {
-        /**
-         * Loads an account's info (public keys) into this.user_data
-         * then returns it. Automatically caches the data
-         * @param {bool}    reload  Refresh the account cache
-         * @param {int}     tries   Internal parameter used for retries on failure
-         * @return {Promise.<user_data, str:err>}
-         */
-        var {user_data} = this;
-        var {username} = user_data;
-        log('Loading account data for', username);
-        // If we already have the account loaded, and no refresh was requested
-        // just use the cache.
-        if(('auths' in user_data) && !reload) {
-            return new Promise((resolve) => {
-                return resolve(user_data);
-            });
-        }
-        return new Promise((resolve, reject) => {
-            steem.api.getAccounts([username], (err, res) => {
-                if(err) {
-                    console.error('A problem occurred while locating the account', username);
-                    console.error('Most likely the RPC node is down.');
-                    var msg = ('message' in err) ? err.message : err;
-                    console.error('The error returned was:', msg);
-                    if(tries < retry_conf.login_attempts) {
-                        console.error(`Will retry in ${retry_conf.login_delay} seconds`);
-                        return delay(retry_conf.login_delay * 1000)
-                          .then(() => resolve(this.loadAccount(reload, tries+1)))
-                          .catch((e) => reject(e));
-                    }
-                    console.error(`Giving up. Tried ${tries} times`)
-                    return reject(`Failed to log in after ${tries} attempts.`);
-                }
-                log(`Successfully connected. Getting data for ${username}`);
-                if(res.length < 1) {
-                    console.error(`ERROR: Account ${username} wasn't found.`);
-                    return reject('account not found');
-                }
-                var account = res[0];
-                // load the public keys from the account
-                var ud = {
-                    ...user_data,
-                    auths: {
-                        owner: account.owner.key_auths[0][0],
-                        active: account.active.key_auths[0][0],
-                        posting: account.posting.key_auths[0][0]
-                    },
-                };
-                this.user_data = ud;
-                return resolve(ud);
-            });
-        });
-    }
+//     loadAccount(reload=false, tries=0) {
+//         /**
+//          * Loads an account's info (public keys) into this.user_data
+//          * then returns it. Automatically caches the data
+//          * @param {bool}    reload  Refresh the account cache
+//          * @param {int}     tries   Internal parameter used for retries on failure
+//          * @return {Promise.<user_data, str:err>}
+//          */
+//         var {user_data} = this;
+//         var {username} = user_data;
+//         log('Loading account data for', username);
+//         // If we already have the account loaded, and no refresh was requested
+//         // just use the cache.
+//         if(('auths' in user_data) && !reload) {
+//             return new Promise((resolve) => {
+//                 return resolve(user_data);
+//             });
+//         }
+//         return new Promise((resolve, reject) => {
+//             steem.api.getAccounts([username], (err, res) => {
+//                 if(err) {
+//                     console.error('A problem occurred while locating the account', username);
+//                     console.error('Most likely the RPC node is down.');
+//                     var msg = ('message' in err) ? err.message : err;
+//                     console.error('The error returned was:', msg);
+//                     if(tries < retry_conf.login_attempts) {
+//                         console.error(`Will retry in ${retry_conf.login_delay} seconds`);
+//                         return delay(retry_conf.login_delay * 1000)
+//                           .then(() => resolve(this.loadAccount(reload, tries+1)))
+//                           .catch((e) => reject(e));
+//                     }
+//                     console.error(`Giving up. Tried ${tries} times`)
+//                     return reject(`Failed to log in after ${tries} attempts.`);
+//                 }
+//                 log(`Successfully connected. Getting data for ${username}`);
+//                 if(res.length < 1) {
+//                     console.error(`ERROR: Account ${username} wasn't found.`);
+//                     return reject('account not found');
+//                 }
+//                 var account = res[0];
+//                 // load the public keys from the account
+//                 var ud = {
+//                     ...user_data,
+//                     auths: {
+//                         owner: account.owner.key_auths[0][0],
+//                         active: account.active.key_auths[0][0],
+//                         posting: account.posting.key_auths[0][0]
+//                     },
+//                 };
+//                 this.user_data = ud;
+//                 return resolve(ud);
+//             });
+//         });
+//     }
 
-    login(reload=false) {
-        /**
-         * Checks if an account + active private key match.
-         * Resolves with user data they do, rejects if there's a problem
-         */
-        if(this.wif_valid) {
-            return new Promise((resolve) => resolve(this.user_data))
-        }
-        return new Promise((resolve, reject) => {
-            this.loadAccount(reload).then((user_data) => {
-                var {username, active_wif, auths} = user_data;
-                var is_valid = steem.auth.wifIsValid(active_wif, auths.active);
-                if(is_valid) {
-                    this.wif_valid = true;
-                    return resolve(user_data);
-                }
-                return reject('Private key WIF does not match key on account')
-            }).catch((e) => reject(e));
-        });
-    }
-    publish_feed(rate, tries=0) {
-        try {
-            // var tr = new TransactionBuilder();
-            var ex_data = rate.toFixed(3) + ` ${config.base_symbol}`;
-            var quote = 1;
-            if(config.peg) {
-                var pcnt = ((1 - config.peg_multi) * 100).toFixed(2);
-                log('Pegging is enabled. Reducing price by '+pcnt+'% (set config.peg to false to disable)');
-                log('Original price (pre-peg):', ex_data);
-                quote = 1 / config.peg_multi;
-            }
+//     login(reload=false) {
+//         /**
+//          * Checks if an account + active private key match.
+//          * Resolves with user data they do, rejects if there's a problem
+//          */
+//         if(this.wif_valid) {
+//             return new Promise((resolve) => resolve(this.user_data))
+//         }
+//         return new Promise((resolve, reject) => {
+//             this.loadAccount(reload).then((user_data) => {
+//                 var {username, active_wif, auths} = user_data;
+//                 var is_valid = steem.auth.wifIsValid(active_wif, auths.active);
+//                 if(is_valid) {
+//                     this.wif_valid = true;
+//                     return resolve(user_data);
+//                 }
+//                 return reject('Private key WIF does not match key on account')
+//             }).catch((e) => reject(e));
+//         });
+//     }
+//     publish_feed(rate, tries=0) {
+//         try {
+//             // var tr = new TransactionBuilder();
+//             var ex_data = rate.toFixed(3) + ` ${config.base_symbol}`;
+//             var quote = 1;
+//             if(config.peg) {
+//                 var pcnt = ((1 - config.peg_multi) * 100).toFixed(2);
+//                 log('Pegging is enabled. Reducing price by '+pcnt+'% (set config.peg to false to disable)');
+//                 log('Original price (pre-peg):', ex_data);
+//                 quote = 1 / config.peg_multi;
+//             }
             
-            var exchangeRate = {base: ex_data, quote: quote.toFixed(3) + ` ${config.quote_symbol}`};
-            var {username, active_wif} = this.user_data;
-            steem.broadcast.feedPublish(active_wif, username, exchangeRate, 
-                (err, r) => {
-                    if(err) {
-                        console.error('Failed to publish feed...');
-                        var msg = ('message' in err) ? err.message : err;
-                        console.error('reason:', msg);
-                        if(tries < retry_conf.feed_attempts) {
-                            console.error(`Will retry in ${retry_conf.feed_delay} seconds`);
-                            return delay(retry_conf.feed_delay * 1000)
-                              .then(() => this.publish_feed(rate, tries+1))
-                              .catch(console.error);
-                        }
-                        console.error(`Giving up. Tried ${tries} times`);
-                        return reject(err);                 
-                    }
-                    log('Data published at: ', ""+new Date());
-                    log('Successfully published feed.');
-                    log(`TXID: ${r.id} TXNUM: ${r.trx_num}`);
-                });
-        } catch(e) {
-            console.error(e);
-        }
-        console.log();
-    }
-}
+//             var exchangeRate = {base: ex_data, quote: quote.toFixed(3) + ` ${config.quote_symbol}`};
+//             var {username, active_wif} = this.user_data;
+//             steem.broadcast.feedPublish(active_wif, username, exchangeRate, 
+//                 (err, r) => {
+//                     if(err) {
+//                         console.error('Failed to publish feed...');
+//                         var msg = ('message' in err) ? err.message : err;
+//                         console.error('reason:', msg);
+//                         if(tries < retry_conf.feed_attempts) {
+//                             console.error(`Will retry in ${retry_conf.feed_delay} seconds`);
+//                             return delay(retry_conf.feed_delay * 1000)
+//                               .then(() => this.publish_feed(rate, tries+1))
+//                               .catch(console.error);
+//                         }
+//                         console.error(`Giving up. Tried ${tries} times`);
+//                         return reject(err);                 
+//                     }
+//                     log('Data published at: ', ""+new Date());
+//                     log('Successfully published feed.');
+//                     log(`TXID: ${r.id} TXNUM: ${r.trx_num}`);
+//                 });
+//         } catch(e) {
+//             console.error(e);
+//         }
+//         console.log();
+//     }
+// }
+
+// try {
+//     var accountmgr = new SteemAcc(config.name, config.wif);
+// } catch(e) {
+//     console.error('A serious error occurred while checking your account:', e);
+//     process.exit(1);
+// }
 
 try {
-    var accountmgr = new SteemAcc(config.name, config.wif);
+    var accountmgr = new Account(config.name, config.wif, config.network, config.nodes);
 } catch(e) {
     console.error('A serious error occurred while checking your account:', e);
     process.exit(1);
@@ -195,7 +209,7 @@ function main() {
         log(`${cap_sym}/${cap_comp} is: ${price.toFixed(3)} ${cap_comp} per ${cap_sym}`);
         log('Attempting to publish feed...');
         if(!dryRun) {
-            accountmgr.publish_feed(price);
+            accountmgr.publish_feed_set_props(price);
         } else {
             console.log('Dry Run. Not actually publishing.');
         }
