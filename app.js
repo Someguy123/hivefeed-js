0e74e3baa34660981b9b9350a4e5fd3320ed1793
Rishi Panthee <rrpanthe@ncsu.edu>
2021-03-21 03:02:12 -0500

    Publish price feed with signing key

--- app.js
+++ app.js
@@ -26,36 +26,37 @@ Alternate Nodes: ${config.alternate_nodes.join(", ")}`);
 console.log('-------------');
 
 global.verbose = false;
-for(var t_arg of process.argv) {
-    if(t_arg == '-v') {
+for (var t_arg of process.argv) {
+    if (t_arg == '-v') {
         global.verbose = true;
     }
 }
 
-hive.api.setOptions({ url: config.node });
+hive.api.setOptions({url: config.node});
 
 // used for re-trying failed promises
 function delay(t) {
-    return new Promise((r_resolve) => setTimeout(r_resolve, t) );
+    return new Promise((r_resolve) => setTimeout(r_resolve, t));
 }
 
 class HiveAcc {
-    constructor(username, active_wif) {
+    constructor(username, active_wif, signing_keys) {
         /**
          * Initialises object with username and active private key
-         * @param  {string}  username    The username (without @)
-         * @param  {string}  active_wif  The active private key
-         * @throws {Error<string:msg>}   If private key is invalid
+         * @param  {string}  username     The username (without @)
+         * @param  {string}  active_wif   The active private key
+         * @param  {array}  signing_keys  Array of signing keys for account
+         * @throws {Error<string:msg>}    If private key is invalid
          */
-        if(!hive.auth.isWif(config.wif)) {
+
+        if (!hive.auth.isWif(config.wif) && !Object.keys(signing_keys).length) {
             throw new Error('The private key you specified is not valid. Be aware Hive private keys start with a 5.');
         }
-        this.user_data = {username, active_wif};
+        this.user_data = {username, active_wif, signing_keys};
         this.wif_valid = null;
-        
     }
 
-    loadAccount(reload=false, tries=0) {
+    loadAccount(reload = false, tries = 0) {
         /**
          * Loads an account's info (public keys) into this.user_data
          * then returns it. Automatically caches the data
@@ -68,104 +69,218 @@ class HiveAcc {
         log(`Loading account data for ${username}`);
         // If we already have the account loaded, and no refresh was requested
         // just use the cache.
-        if(('auths' in user_data) && !reload) {
+        if (('auths' in user_data) && !reload) {
             return new Promise((resolve) => {
                 return resolve(user_data);
             });
         }
         return new Promise((resolve, reject) => {
             hive.api.getAccounts([username], (err, res) => {
-                if(err) {
+                if (err) {
                     console.error(`A problem occurred while locating the account ${username}`);
                     console.error('Most likely the RPC node is down.');
                     var msg = ('message' in err) ? err.message : err;
                     console.error('The error returned was:', msg);
-                    if(tries < retry_conf.login_attempts) {
-                        switchNode()
+                    if (tries < retry_conf.login_attempts) {
+                        switchNode();
                         console.error(`Will retry in ${retry_conf.login_delay} seconds`);
                         return delay(retry_conf.login_delay * 1000)
-                          .then(() => resolve(this.loadAccount(reload, tries+1)))
-                          .catch((e) => reject(e));
+                            .then(() => resolve(this.loadAccount(reload, tries + 1)))
+                            .catch((e) => reject(e));
                     }
-                    console.error(`Giving up. Tried ${tries} times`)
+                    console.error(`Giving up. Tried ${tries} times`);
                     return reject(`Failed to log in after ${tries} attempts.`);
                 }
                 log(`Successfully connected. Getting data for ${username}`);
-                if(res.length < 1) {
+                if (res.length < 1) {
                     console.error(`ERROR: Account ${username} wasn't found.`);
                     return reject('account not found');
                 }
                 var account = res[0];
-                // load the public keys from the account
-                var ud = {
-                    ...user_data,
-                    auths: {
-                        owner: account.owner.key_auths[0][0],
-                        active: account.active.key_auths[0][0],
-                        posting: account.posting.key_auths[0][0]
-                    },
-                };
-                this.user_data = ud;
-                return resolve(ud);
+                // load the signing key from the account
+                hive.api.getWitnessByAccount(username, (err, result) => {
+                    if (err) {
+                        console.error(`A problem occurred while locating the witness ${username}`);
+                        console.error('Most likely the RPC node is down.');
+                        var msg = ('message' in err) ? err.message : err;
+                        console.error('The error returned was:', msg);
+                        if (tries < retry_conf.login_attempts) {
+                            switchNode();
+                            console.error(`Will retry in ${retry_conf.login_delay} seconds`);
+                            return delay(retry_conf.login_delay * 1000)
+                                .then(() => resolve(this.loadAccount(reload, tries + 1)))
+                                .catch((e) => reject(e));
+                        }
+                        console.error(`Giving up. Tried ${tries} times`);
+                        return reject(`Failed to log in after ${tries} attempts.`);
+                    }
+                    if (!result) {
+                        console.error(`A problem occurred while locating the witness ${username}`);
+                        console.error('Most likely the account is not a witness.');
+                        if (tries < retry_conf.login_attempts) {
+                            switchNode();
+                            console.error(`Will retry in ${retry_conf.login_delay} seconds`);
+                            return delay(retry_conf.login_delay * 1000)
+                                .then(() => resolve(this.loadAccount(reload, tries + 1)))
+                                .catch((e) => reject(e));
+                        }
+                        console.error(`Giving up. Tried ${tries} times`);
+                        return reject(`Failed to log in after ${tries} attempts.`);
+                    }
+                    var ud = {
+                        ...user_data,
+                        auths: {
+                            owner: account.owner.key_auths[0][0],
+                            active: account.active.key_auths[0][0],
+                            posting: account.posting.key_auths[0][0],
+                            signing: result.signing_key
+                        },
+                    };
+                    this.user_data = ud;
+                    return resolve(ud);
+                });
             });
         });
     }
 
-    login(reload=false) {
+    login(reload = false) {
         /**
          * Checks if an account + active private key match.
          * Resolves with user data they do, rejects if there's a problem
          */
-        if(this.wif_valid) {
+        if (this.wif_valid) {
             return new Promise((resolve) => resolve(this.user_data))
         }
         return new Promise((resolve, reject) => {
             this.loadAccount(reload).then((user_data) => {
-                var {username, active_wif, auths} = user_data;
-                var is_valid = hive.auth.wifIsValid(active_wif, auths.active);
-                if(is_valid) {
-                    this.wif_valid = true;
+                var {username, active_wif, signing_keys, auths} = user_data;
+                var is_valid = false;
+                try {
+                    is_valid = hive.auth.wifIsValid(active_wif, auths.active);
+                    if (is_valid) {
+                        this.wif_valid = true;
+                    }
+                } catch (e) {
+
+                }
+                try {
+                    if (signing_keys.hasOwnProperty(auths.signing)) {
+                        this.signing_valid = true;
+                    }
+                } catch (e) {
+
+                }
+                if (this.wif_valid || this.signing_valid) {
                     return resolve(user_data);
                 }
-                return reject('Private key WIF does not match key on account')
+                return reject('Private key WIF does not match key on account nor do the signing keys');
             }).catch((e) => reject(e));
         });
     }
-    publish_feed(rate, tries=0) {
+
+    publish_feed(rate, tries = 0) {
         try {
             // var tr = new TransactionBuilder();
             var ex_data = rate.toFixed(3) + ` ${config.base_symbol}`;
             var quote = 1;
-            if(config.peg) {
+            if (config.peg) {
                 var pcnt = ((1 - config.peg_multi) * 100).toFixed(2);
                 log(`Pegging is enabled. Reducing price by ${pcnt}% (set config.peg to false to disable)`);
                 log(`Original price (pre-peg): ${ex_data}`);
                 quote = 1 / config.peg_multi;
             }
-            
+
             var exchangeRate = {base: ex_data, quote: quote.toFixed(3) + ` ${config.quote_symbol}`};
-            var {username, active_wif} = this.user_data;
-            hive.broadcast.feedPublish(active_wif, username, exchangeRate, 
-                (err, r) => {
-                    if(err) {
-                        console.error('Failed to publish feed...');
-                        var msg = ('message' in err) ? err.message : err;
-                        console.error(`reason: ${msg}`);
-                        if(tries < retry_conf.feed_attempts) {
-                            switchNode()
-                            console.error(`Will retry in ${retry_conf.feed_delay} seconds`);
-                            return delay(retry_conf.feed_delay * 1000)
-                              .then(() => this.publish_feed(rate, tries+1))
-                              .catch(console.error);
+            var {username, active_wif, signing_keys} = this.user_data;
+            if (this.signing_valid) {
+                hive.api.getWitnessByAccount(username, (err, result) => {
+                        if (err) {
+                            console.error(`A problem occurred while getting current signing key`);
+                            console.error('Most likely the RPC node is down.');
+                            var msg = ('message' in err) ? err.message : err;
+                            console.error('The error returned was:', msg);
+                            if (tries < retry_conf.feed_attempts) {
+                                switchNode();
+                                console.error(`Will retry in ${retry_conf.feed_delay} seconds`);
+                                return delay(retry_conf.feed_attempts * 1000)
+                                    .then(() => resolve(this.publish_feed(feed, tries + 1)))
+                                    .catch((e) => reject(e));
+                            }
+                            console.error(`Giving up. Tried ${tries} times`);
+                            return reject(`Failed to publish in after ${tries} attempts.`);
                         }
-                        console.error(`Giving up. Tried ${tries} times`);
-                        return reject(err);                 
+                        if (!result) {
+                            console.error(`A problem occurred while getting current signing key`);
+                            console.error('Most likely the account is not a witness.');
+                            if (tries < retry_conf.feed_attempts) {
+                                switchNode();
+                                console.error(`Will retry in ${retry_conf.feed_delay} seconds`);
+                                return delay(retry_conf.feed_attempts * 1000)
+                                    .then(() => resolve(this.publish_feed(feed, tries + 1)))
+                                    .catch((e) => reject(e));
+                            }
+                            console.error(`Giving up. Tried ${tries} times`);
+                            return reject(`Failed to publish in after ${tries} attempts.`);
+                        }
+                        let signing = result.signing_key;
+                        if (signing_keys.hasOwnProperty(signing)) {
+                            let props = {
+                                "key": signing,
+                                "hbd_exchange_rate": {"base": exchangeRate.base, "quote": exchangeRate.quote}
+                            };
+                            let op = hive.utils.buildWitnessUpdateOp(username, props);
+                            hive.broadcast.witnessSetProperties(signing_keys[signing], username, op[1].props, [], (err, result) => {
+                                if (err) {
+                                    console.error('Failed to publish feed...');
+                                    var msg = ('message' in err) ? err.message : err;
+                                    console.error(`reason: ${msg}`);
+                                    if (tries < retry_conf.feed_attempts) {
+                                        switchNode();
+                                        console.error(`Will retry in ${retry_conf.feed_delay} seconds`);
+                                        return delay(retry_conf.feed_delay * 1000)
+                                            .then(() => this.publish_feed(rate, tries + 1))
+                                            .catch(console.error);
+                                    }
+                                    console.error(`Giving up. Tried ${tries} times`);
+                                    return reject(err);
+                                }
+
+                                log(`Data published at: ${new Date()}`); //Not cure if this one will work
+                                log('Successfully published feed.');
+                                log(`TXID: ${result.id} TXNUM: ${result.trx_num}`);
+                            });
+                        } else {
+                            this.signing_valid = false;
+                            return delay(retry_conf.feed_attempts * 1000)
+                                .then(() => resolve(this.publish_feed(feed, tries + 1)))
+                                .catch((e) => reject(e));
+                        }
                     }
-                    log(`Data published at: ${new Date()}`); //Not cure if this one will work
-                    log('Successfully published feed.');
-                    log(`TXID: ${r.id} TXNUM: ${r.trx_num}`);
-                });
-        } catch(e) {
+                );
+            } else {
+                hive.broadcast.feedPublish(active_wif, username, exchangeRate,
+                    (err, r) => {
+                        if (err) {
+                            console.error('Failed to publish feed...');
+                            var msg = ('message' in err) ? err.message : err;
+                            console.error(`reason: ${msg}`);
+                            if (tries < retry_conf.feed_attempts) {
+                                switchNode();
+                                console.error(`Will retry in ${retry_conf.feed_delay} seconds`);
+                                return delay(retry_conf.feed_delay * 1000)
+                                    .then(() => this.publish_feed(rate, tries + 1))
+                                    .catch(console.error);
+                            }
+                            console.error(`Giving up. Tried ${tries} times`);
+                            return reject(err);
+                        }
+                        log(`Data published at: ${new Date()}`); //Not cure if this one will work
+                        log('Successfully published feed.');
+                        log(`TXID: ${r.id} TXNUM: ${r.trx_num}`);
+                    });
+            }
+        } catch
+            (e) {
             console.error(e);
         }
         console.log();
@@ -173,8 +288,8 @@ class HiveAcc {
 }
 
 try {
-    var accountmgr = new HiveAcc(config.name, config.wif);
-} catch(e) {
+    var accountmgr = new HiveAcc(config.name, config.wif, config.signing_keys);
+} catch (e) {
     console.error(`A serious error occurred while checking your account: ${e}`);
     process.exit(1);
 }
@@ -182,23 +297,23 @@ try {
 var shouldPublish = settings.shouldPublish;
 var dryRun = settings.dryRun;
 
-var cap_sym = config.ex_symbol.toUpperCase(), 
+var cap_sym = config.ex_symbol.toUpperCase(),
     cap_comp = config.ex_compare.toUpperCase();
 
 function get_price(callback) {
-    exchange.get_pair( config.ex_symbol, config.ex_compare,
+    exchange.get_pair(config.ex_symbol, config.ex_compare,
         (err, price) => callback(err, parseFloat(price))
     );
 }
 
 function main() {
-    get_price((err,price) => {
-        if(err) {
+    get_price((err, price) => {
+        if (err) {
             return console.error('error loading prices, will retry later');
         }
         log(`${cap_sym}/${cap_comp} is: ${price.toFixed(3)} ${cap_comp} per ${cap_sym}`);
         log('Attempting to publish feed...');
-        if(!dryRun) {
+        if (!dryRun) {
             accountmgr.publish_feed(price);
         } else {
             console.log('Dry Run. Not actually publishing.');
@@ -206,9 +321,9 @@ function main() {
     });
 }
 
-function switchNode(){
+function switchNode() {
     let currentNode = config.alternate_nodes.shift()
-    hive.api.setOptions({ url: currentNode })
+    hive.api.setOptions({url: currentNode})
     config.alternate_nodes.push(currentNode)
     console.log(`Switched node to ${currentNode}.`)
 }
